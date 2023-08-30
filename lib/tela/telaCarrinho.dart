caf9a92995d9190da6d7935aeb19b1b14dd40f1a
LetyciaEst08 <letyciaesteves@hotmail.com>
2023-08-30 18:41:02 -0300

    alteração no botão de sair do carrinho

--- lib/tela/telaCarrinho.dart
+++ lib/tela/telaCarrinho.dart
@@ -15,48 +15,66 @@ class _TelaCarrinhoState extends State<TelaCarrinho> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Carrinho de Compras'),
+        title: const Text('Carrinho de Compras'),
         backgroundColor: Colors.pink, // Cor de fundo pink
       ),
-      body: ListView.builder(
-        itemCount: widget.carrinho.length,
-        itemBuilder: (context, index) {
-          final produto = widget.carrinho[index];
-          return ListTile(
-            leading: Image.asset(produto.imagem),
-            title: Text(produto.nome),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Preço: R\$ ${produto.preco}'),
-                Text('Descrição: ${produto.descricao}'),
-                Text('Quantidade: ${produto.quantidade}'),
-              ],
-            ),
-            trailing: Row(  //propriedade para definir um widget que aparece ao lado dos itens representados
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    //logica para exluir produto do carrinho
-                    setState(() {
-                      widget.carrinho.removeAt(index);
-                    });
-                  },
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+      body: widget.carrinho.isEmpty
+          ?Center(
+            child: Text(
+              'Seu carrinho está vazio!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w100,
+              ),
+              textAlign: TextAlign.center,
+              ),
+          )
+        : ListView.builder(
+          itemCount: widget.carrinho.length,
+          itemBuilder: (context, index) {
+            final produto = widget.carrinho[index];
+            return ListTile(
+              leading: Image.asset(produto.imagem),
+              title: Text(produto.nome),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Preço: R\$ ${produto.preco}'),
+                  Text('Descrição: ${produto.descricao}'),
+                  Text('Quantidade: ${produto.quantidade}'),
+                ],
+              ),
+              trailing: Row(  //propriedade para definir um widget que aparece ao lado dos itens representados
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      //logica para exluir produto do carrinho
+                      setState(() {
+                        widget.carrinho.removeAt(index);
+                      });
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+           if (widget.carrinho.isEmpty) {
+      // Se o carrinho estiver vazio, retornar à tela inicial
+      Navigator.popUntil(context, (route) => route.isFirst);
+    } else {
           mostrarModalExclusao(context);
           setState(() {
             widget.carrinho
                 .clear(); // Limpar a lista de carrinho após a confirmação
-          });
+            });
+          }
         },
         child: Icon(Icons.clear),
       ), // Chamar a função do modal ao pressionar o botão
